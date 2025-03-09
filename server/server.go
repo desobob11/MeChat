15b0e7ba98d45f146940d82cabccae742607d4d7
desobob11 <desmond.obrien@ucalgary.ca>
2025-03-09 12:07:17 -0600

    mod for server

--- server/server.go
+++ server/server.go
@@ -1,53 +1,35 @@
 package main
 
 import (
-    "bufio"
+   // "bufio"
     "fmt"
-    "log"
-    "net"
-    "strings"
+   // "log"
+   // "net"
+   // "strings"
+  //  "net/rpc"
+    _ "modernc.org/sqlite"
+    "database/sql"
 )
 
-func handleConnection(conn net.Conn, cons *[]net.Conn) {
-    defer conn.Close() // Ensure the connection is closed when the function finishes
 
-    fmt.Println(cons)
-    for {
-        message, err := bufio.NewReader(conn).ReadString('\n')
-        if err != nil {
-            log.Println("Connection error:", err)
-            return // Exit the loop if there is an error reading from the connection
-        }
 
-        fmt.Print("Message Received: ", message)
-        newMessage := strings.ToUpper(message)
-        for _, i := range *cons {
-            if i != conn {
-                i.Write([]byte(newMessage + "\n"))
-            }
-        }
-    }
-}
+
 
 func main() {
+    // Open (or create) the database file
 
-    cons := new([]net.Conn)
-
-    ln, err := net.Listen("tcp", "localhost:8000")
+    var result sql.Result
+    db, err := sql.Open("sqlite", "mydatabase.sqlite")
     if err != nil {
-        log.Fatal(err)
+        fmt.Println("Error opening database:", err)
+        return
     }
-    fmt.Println("Listening on port 8000")
+    defer db.Close()
 
-    // Accept and handle multiple connections in goroutines
-    for {
-        conn, err := ln.Accept()
-        if err != nil {
-            log.Println("Connection accept error:", err)
-            continue // Continue accepting new connections even if one fails
-        }
-        *cons = append(*cons, conn)
-        // Handle each connection concurrently
-        go handleConnection(conn, cons)
-    }
+    // Create a table
+    createTableSQL := `SELECT * FROM users;`
+    result, err = db.Exec(createTableSQL)
+
+    fmt.Println(result.RowsAffected())
+    
 }
