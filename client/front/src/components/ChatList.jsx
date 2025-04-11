861fcaf4be1d451fc0adfaf766608ec3b358de00
desobob11 <desmond.obrien@ucalgary.ca>
2025-04-11 15:50:33 -0600

    searchable users list

--- client/front/src/components/ChatList.jsx
+++ client/front/src/components/ChatList.jsx
@@ -10,7 +10,7 @@ export const ChatContact = (props) => {
         <li key={props.email} className="flex justify-between gap-x-6 p-5 border-solid border-b-1 ">
             <div className="flex min-w-0 gap-x-4">
 
-               
+
                 <div className="min-w-0 flex-auto">
                     <p className="text-sm/6 font-semibold text-gray-900">{props.name}</p>
                     <p className="mt-1 truncate text-xs/5 text-gray-500">{props.email}</p>
@@ -25,7 +25,7 @@ export const ChatContact = (props) => {
 
 export const NameListItem = (props) => {
 
-    
+
     const { userProfile, setUserProfile } = useGlobal()
 
     const sendCreateContactMessage = () => {
@@ -39,15 +39,15 @@ export const NameListItem = (props) => {
             body: JSON.stringify(req_body),
         };
         fetch(`http://127.0.0.1:${BACK_END_PORT}/${ADD_CONTACT_ROUTE}`, options)
-        .then(response => {
-            if (!response.ok) {
-                alert("Error adding friend. You may already have them as a friend. Or try again later")
-            }
-            else {
-                alert("Friend added!")
-            }
-     
-        })
+            .then(response => {
+                if (!response.ok) {
+                    alert("Error adding friend. You may already have them as a friend. Or try again later")
+                }
+                else {
+                    alert("Friend added!")
+                }
+
+            })
 
         props.refreshFunction();
     }
@@ -56,7 +56,7 @@ export const NameListItem = (props) => {
         <button onClick={sendCreateContactMessage} className='w-full text-left hover:bg-gray-100 active:bg-white'>
             <div>
                 <p className="grid grid-cols-1 text-gray-800 text-xl font-bold font-sans ">
-                    {props.UserId}. {props.Firstname} {props.Lastname}
+                    {props.Firstname} {props.Lastname}
                 </p>
                 <p className="paddingtext-gray-800 text-sm font-sans  ">
                     {props.Email}
@@ -95,40 +95,40 @@ export const NameList = (props) => {
                 <p className="paddingtext-gray-800 text-4xl font-sans font-bold ">
                     Add
                 </p>
-             
+
                 <button onClick={props.buttonAction} className="h-10 w-10 text-white bg-gradient-to-b from-red-400 to-red-500 rounded-full flex items-center justify-center">
-                    <XMarkIcon  className="w-6 h-6" />
+                    <XMarkIcon className="w-6 h-6" />
                 </button>
                 <form onChange={handleInputChange} onSubmit={x => { }}>
-                <div class="grid grid-cols-2">
-                    <input  type="text"
-                        id="first_name"
-                        className="w-[100%] border border-gray-300 mt-2 p-2 h-10 bg-gray-white text-gray-600 rounded-3xl w-64"
-                        placeholder="Search Users"
-                        required
-                        value={currentInput}
-                        autoComplete="off" />
+                    <div class="grid grid-cols-2">
+                        <input type="text"
+                            id="first_name"
+                            className="w-[100%] border border-gray-300 mt-2 p-2 h-10 bg-gray-white text-gray-600 rounded-3xl w-72"
+                            placeholder="Search Users"
+                            required
+                            value={currentInput}
+                            autoComplete="off" />
 
-                </div>
+                    </div>
 
-            </form>
+                </form>
             </div>
 
             <ul role="list"
                 className="overflow-auto h-[calc(100%-64px)] px-4 py-2 w-full flex flex-col gap-y-4">
 
-             
-                    {usersToDisplay.map((item, index) => (
-                        <NameListItem
-                            key={index}
-                            refreshFunction={props.refreshFunction}
-                            Firstname={item.Firstname}
-                            Lastname={item.Lastname}
-                            Email={item.Email}
-                            UserId={item.UserId}
-                        />
-                    ))}
-                
+
+                {usersToDisplay.map((item, index) => (
+                    <NameListItem
+                        key={index}
+                        refreshFunction={props.refreshFunction}
+                        Firstname={item.Firstname}
+                        Lastname={item.Lastname}
+                        Email={item.Email}
+                        UserId={item.UserId}
+                    />
+                ))}
+
             </ul>
 
 
@@ -146,11 +146,30 @@ export default function ChatList() {
 
     const { userProfile, setUserProfile } = useGlobal()
 
-    const [contacts, setContacts] = useState(null)
+    const [contacts, setContacts] = useState(null);
+    const [searchContacts, setSearchContacts] = useState([]);
     const [nameListVisible, setNameListVisible] = useState(false);
     const { selectedContactId, setSelectedContactId } = useGlobal();
-
+    const [chatSearchText, setChatSearchText] = useState("")
     const { allUsers, setAllUsers } = useGlobal();
+
+    
+    // need to continuously check for use
+    useEffect(() => {
+        const interval = setInterval(() => {
+            GetContacts();
+            getAllUsers();
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+
+
+    const handleInputChange = (e) => {
+        setChatSearchText(e.target.value);
+        setSearchContacts(contacts.filter((user) => `${user.Firstname}${user.Lastname}${user.Email}`.toLowerCase().includes(e.target.value.toLowerCase())))
+    }
+
 
     const openNameList = () => {
         setNameListVisible(true);
@@ -160,8 +179,6 @@ export default function ChatList() {
         setNameListVisible(false);
     }
 
-
-    
     const getAllUsers = () => {
         var req_body = {
             UserId: userProfile.UserId,
@@ -216,15 +233,6 @@ export default function ChatList() {
 
 
 
-    // need to continuously check for use
-    useEffect(() => {
-        const interval = setInterval(() => {
-            GetContacts();
-            getAllUsers();
-        }, 1000); 
-        return () => clearInterval(interval);
-    }, []);
-
 
     return (
         <div className="w-2/4 h-4/4">
@@ -232,10 +240,23 @@ export default function ChatList() {
             <text className="paddingtext-gray-800 text-4xl font-sans font-bold ">
                 Chats
             </text>
-          
-            <button onClick={setNameListVisible} className="flex items-center justify-center ml-2 mt-2 h-10 w-[25%] text-white bg-gradient-to-b from-blue-400 to-blue-500 rounded-3xl">
-                <PlusIcon  className="w-6 h-6" />
-            </button>
+            <div class="grid grid-cols-2">
+                <button onClick={setNameListVisible} className="flex items-center justify-center ml-2 mt-2 h-10 w-[25%] w-16 text-white bg-gradient-to-b from-blue-400 to-blue-500 rounded-3xl">
+                    <PlusIcon className="w-6 h-6" />
+                </button>
+                <form onChange={handleInputChange} onSubmit={x => { }}>
+
+                    <input type="text"
+                        id="first_name"
+                        className="w-[100%] border border-gray-300 mt-2 p-2 h-10 bg-gray-white text-gray-600 rounded-3xl w-74"
+                        placeholder="Search Users"
+                        required
+                        value={chatSearchText}
+                        autoComplete="off" />
+                </form>
+            </div>
+
+
 
             {contacts === null ? (
                 <p>Loading...</p>
@@ -243,20 +264,23 @@ export default function ChatList() {
                 [<p>No contacts found</p>]
             ) : (
                 <ul role="list" className="divide-y divide-gray-100 border-solid shadow-md rounded-xl h-96 overflow-auto">
-                    {contacts.map((person) => (
-                        <button className="block w-full text-left hover:bg-gray-100 bg-transparent border-0 p-0 m-0 outline-none focus:outline-none"
-                            onClick={() => setSelectedContactId(person.UserId)}>
-                            <ChatContact
-                                userid={person.UserId}
-                                name={`${person.Firstname} ${person.Lastname}`}
-                                email={person.Email}
-                                role={person.Descr}
-                                lastSeen={""}
-                                lastSeenDateTime={""}
+                    {
+                        searchContacts.map((person) => (
+                            <button className="block w-full text-left hover:bg-gray-100 bg-transparent border-0 p-0 m-0 outline-none focus:outline-none"
+                                onClick={() => setSelectedContactId(person.UserId)}>
+                                <ChatContact
+                                    userid={person.UserId}
+                                    name={`${person.Firstname} ${person.Lastname}`}
+                                    email={person.Email}
+                                    role={person.Descr}
+                                    lastSeen={""}
+                                    lastSeenDateTime={""}
 
-                            />
-                        </button>
-                    ))}
+                                />
+                            </button>
+                        ))
+                
+                    }
                 </ul>
             )}
 
